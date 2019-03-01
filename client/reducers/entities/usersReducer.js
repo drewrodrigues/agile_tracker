d10c2dd6978848887b74d65bd35fb6261167ba25
Andrew <thesimpledev@gmail.com>
2019-03-01 13:49:00 -0800

    Fix sign in issue

--- client/reducers/entities/usersReducer.js
+++ client/reducers/entities/usersReducer.js
@@ -1,10 +1,13 @@
 import { SIGN_UP_USER } from '../../actions/userActions'
+import { SIGN_IN_USER } from '../../actions/sessionActions'
 
 const usersReducer = (oldState = {}, action) => {
   Object.freeze(oldState)
   let newState = Object.assign({}, oldState)
 
   switch (action.type) {
+    case SIGN_IN_USER:
+      return Object.assign(newState, action.user)
     case SIGN_UP_USER:
       return Object.assign(newState, action.user)
     default:
